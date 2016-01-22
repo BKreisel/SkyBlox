f77ea2b0b70f75ef6fdf9c2967948e4a76fa039c
Brandon Kreisel <kreiselb@gmail.com>
2016-01-22 16:13:16 -0500

    Add Game Info
    Insert Game Info
    Rename Title Function

--- render.py
+++ render.py
@@ -2,12 +2,38 @@ import pygame
 
 from constants import *
 
-def render_title(surface):
+def title(surface):
     font = pygame.font.Font("assets/fonts/Exo-BoldItalic.otf",64)
     title_sky = font.render("Sky",True,WHITE)
     title_blox = font.render("Blox",True, WHITE)
     surface.blit(title_sky,(430,30))
     surface.blit(title_blox,(494,100))
+
+def set_game_info(surface,level_val=1,score_val=0,lines_val=0):
+    font_size = 32
+    font = pygame.font.Font("assets/fonts/Exo-BoldItalic.otf",font_size)
+
+    level_text = "Level: " + str(level_val)
+    score_text = "Score: " + str(score_val)
+    lines_text = "Lines: " + str(lines_val)
+
+    level = font.render(level_text,True,WHITE)
+    score = font.render(score_text,True, WHITE)
+    lines = font.render(lines_text,True, WHITE)
+
+    cover_x = lambda text: len(text) * font_size
+
+    #(Re)draw LEVEL
+    pygame.draw.rect(surface,BLACK,(430,500,cover_x(level_text),font_size+3))
+    surface.blit(level,(430,500))
+
+    #(Re)draw SCORE
+    pygame.draw.rect(surface,BLACK,(430,550,cover_x(score_text),font_size+3))
+    surface.blit(score,(430,550))
+
+    #(Re)draw LINES
+    pygame.draw.rect(surface,BLACK,(430,600,cover_x(lines_text),font_size+3))
+    surface.blit(lines,(430,600))
 
 def game_border(surface):
     pygame.draw.rect(surface, WHITE,
@@ -17,8 +43,9 @@ def game_border(surface):
 def render_screen(surface):
 
     items = [
-        render_title
-        ,game_border
+        title,
+        game_border,
+        set_game_info,
     ]
 
     for item in items:
